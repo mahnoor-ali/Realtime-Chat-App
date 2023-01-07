50f8a872aa186191554f30917762785971a3652a
Mahnoor <mahnoorali57se@gmail.com>
2023-01-08 01:28:48 +0500

    server side completed

--- NodeServer/index.js
+++ NodeServer/index.js
@@ -1,21 +1,32 @@
-const io = require('socket.io')(8000) // import the Socket.IO library and create a new Socket.IO server that listens for connections on port 8000
+// import the Socket.IO library and create a new Socket.IO server that listens for connections on port 8000
+const { Server } = require("socket.io"); 
+
+//below code is to allow cross-origin requests from the client (i.e. the web page) to the server (i.e. the Node.js server) 
+const io = new Server({
+    cors: {
+        origin: "http://127.0.0.1:5500",
+        methods: ["GET", "POST"]
+    }
+});
 
 const users = {};
 
-io.on('connection', socket => {  // set up an event listener for when a new client connects to the server
-  socket.on('new-user-joined', name => {  // set up an event listener for the 'new-user-joined' event, emitted by the client
+io.on('connection', socket => {  
+  // listen for 'connection' event emitted by the server when a new client connects
+    socket.on('new-user-joined', name => {  // set up an event listener for the 'new-user-joined' event, emitted by the client
     users[socket.id] = name;  // add the client's ID and name to the 'users' object
     socket.broadcast.emit('user-joined', name)  // emit a 'user-joined' event to all clients except the one that just connected
   })
-})
 
-socket.on('send', message => {  // set up an event listener for the 'send' event, emitted by the client
-  socket.broadcast.emit('receive', { message: message, name: users[socket.id] })  // emit a 'receive' event to all clients except the one that just sent the message, along with the message and the sender's name
-})
+  socket.on('send', message => {  // set up an event listener for the 'send' event, emitted by the client
+    socket.broadcast.emit('receive', { message: message, name: users[socket.id] })  // emit a 'receive' event to all clients except the one that just sent the message, along with the message and the sender's name
+  })
 
-/*
-NOTES:
-This code sets up a Socket.IO server and listens for connection and message events from the client. 
-When a new client connects, the server adds the client's ID and name to the users object and broadcasts a 'user-joined' event to all
-other clients. When a client sends a message, the server broadcasts a 'receive' event to all other clients, along with the message and the sender's name.
-*/
+  
+  socket.on("disconnect", message => {
+    socket.broadcast.emit("left", users[socket.id]);
+    delete users[socket.id]; //delete client's record from users object
+  });
+  })
+
+  io.listen(8000); // listen for connections on port 8000
