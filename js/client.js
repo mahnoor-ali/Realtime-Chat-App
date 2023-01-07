e79f484e482fe9f7246c021ef4bc047260ac884c
Mahnoor <mahnoorali57se@gmail.com>
2023-01-08 01:33:51 +0500

    sound and time added

--- js/client.js
+++ js/client.js
@@ -6,6 +6,12 @@ const socket = io("http://localhost:8000");
 const inputMsg = document.getElementById("typeMsg");
 const inputForm = document.getElementById("typing-box");
 const messageContainer = document.querySelector(".chat-box"); //container that only have messages of receiver and sender
+var audio = new Audio('tingg.mp3'); //audio file to play when a new user joins or leaves the chat Or a message is received
+var today = new Date();
+var hours = today.getHours();
+var minutes = today.getMinutes();var am_pm = hours >= 12 ? 'PM' : 'AM'; //convert 24 hour format to 12 hour format
+hours = hours%12 ? hours : 12; // the hour '0' should be '12'
+var time = hours + ':' + minutes + ' ' + am_pm;
 
 const userName = prompt("Please enter your name");
 socket.emit('new-user-joined', userName); //send 'new-user-joined'event from the client to server
@@ -41,6 +47,7 @@ function append(message, positionOfMsg)
     joinedMessage.classList.add("message-container");
     joinedMessage.innerHTML= `<div class="message-container ${positionOfMsg}}">
                               <div class="message-bubble"> ${message} 
+                              <p id="time"> ${time} </p>
                               </div>
                               </div>`;
     }
@@ -52,4 +59,5 @@ function append(message, positionOfMsg)
 
     messageContainer.append(joinedMessage); //append the message to chatbox container 
     if(positionOfMsg=="left" || positionOfMsg=="centre") //if the message is of type 'user left'
+      audio.play();
 }
